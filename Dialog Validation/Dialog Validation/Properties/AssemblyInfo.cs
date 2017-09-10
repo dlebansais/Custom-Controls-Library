909939faec0e2da68307bf87943c98056e96db02
dlebansais <dlebansais@gmail.com>
2017-09-10 13:03:00 +0200

    Updated calls to signfile.bat and updateversion.bat

--- Dialog Validation/Dialog Validation/Properties/AssemblyInfo.cs
+++ Dialog Validation/Dialog Validation/Properties/AssemblyInfo.cs
@@ -38,6 +38,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.136")]
-[assembly: AssemblyFileVersion("1.0.0.136")]
+[assembly: AssemblyVersion("1.0.0.137")]
+[assembly: AssemblyFileVersion("1.0.0.137")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
