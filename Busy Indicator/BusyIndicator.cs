640986064bd9fd2b610d93436c88c8aa58bfe09e
dlebansais <dlebansais@gmail.com>
2017-10-09 22:11:34 +0200

    Added Busy Indicator.

---
+++ Busy Indicator/BusyIndicator.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CustomControls
+{
+    public partial class BusyIndicator : UserControl
+    {
+        public BusyIndicator()
+        {
+            InitializeComponent();
+        }
+    }
+}
