c4e54152271378312acf557b014689e7e2017a3e
dlebansais <dlebansais@gmail.com>
2018-02-28 11:31:43 -0800

    Upgraded to VS2017. Updated certificate.

--- Extended ScrollBar/Extended ScrollBar/Properties/AssemblyInfo.cs
+++ Extended ScrollBar/Extended ScrollBar/Properties/AssemblyInfo.cs
@@ -38,6 +38,6 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.138")]
-[assembly: AssemblyFileVersion("1.0.0.35")]
+[assembly: AssemblyVersion("1.0.0.143")]
+[assembly: AssemblyFileVersion("1.0.0.37")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
