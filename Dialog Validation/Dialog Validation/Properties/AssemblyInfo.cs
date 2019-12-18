88d1c2cbf48828e1b9ecd14ca042074298a5beb3
dlebansais <dlebansais@gmail.com>
2019-12-18 17:23:09 +0100

    Updated to Visual Studio 2019.

--- Dialog Validation/Dialog Validation/Properties/AssemblyInfo.cs
+++ Dialog Validation/Dialog Validation/Properties/AssemblyInfo.cs
@@ -38,6 +38,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.158")]
-[assembly: AssemblyFileVersion("1.0.0.39")]
+[assembly: AssemblyVersion("1.0.0.162")]
+[assembly: AssemblyFileVersion("1.0.0.40")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
