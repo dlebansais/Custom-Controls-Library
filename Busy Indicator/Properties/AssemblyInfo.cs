71be6c4ea535adeed752a6538e12aadee90d39fc
dlebansais <dlebansais@gmail.com>
2019-12-24 15:23:00 +0100

    Updated to C# 8.0

--- Busy Indicator/Properties/AssemblyInfo.cs
+++ Busy Indicator/Properties/AssemblyInfo.cs
@@ -54,7 +54,7 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.162")]
-[assembly: AssemblyFileVersion("1.0.0.40")]
+[assembly: AssemblyVersion("1.0.0.221")]
+[assembly: AssemblyFileVersion("1.0.0.41")]
 [assembly: NeutralResourcesLanguage("en")]
 
