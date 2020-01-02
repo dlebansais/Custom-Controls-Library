d75155fd0f82771aa4966d86bda7cb8b13734e92
dlebansais <dlebansais@gmail.com>
2020-01-02 20:54:24 +0100

    Improved Test-BusyIndicator. [skip ci]

--- BusyIndicator/Properties/AssemblyInfo.cs
+++ BusyIndicator/Properties/AssemblyInfo.cs
@@ -19,6 +19,6 @@ using System.Windows.Markup;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.0.0.519")]
-[assembly: AssemblyFileVersion("1.0.0.49")]
+[assembly: AssemblyVersion("1.0.0.523")]
+[assembly: AssemblyFileVersion("1.0.0.50")]
 [assembly: NeutralResourcesLanguage("en-US")]
