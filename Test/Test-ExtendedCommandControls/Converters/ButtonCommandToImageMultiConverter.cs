afc405434f7762e341b81fabea4e8882bb8d920f
dlebansais <dlebansais@gmail.com>
2020-02-20 20:31:39 +0100

    Misc changes.

---
+++ Test/Test-ExtendedCommandControls/Converters/ButtonCommandToImageMultiConverter.cs
@@ -0,0 +1,76 @@
+﻿namespace Converters
+{
+    using CustomControls;
+    using System;
+    using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Input;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// Represents the converter that converts a command to a <see cref="ImageSource"/>.
+    /// </summary>
+    [SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Instantiated in Xaml")]
+    internal class ButtonCommandToImageMultiConverter : IMultiValueConverter
+    {
+        /// <summary>
+        /// Converts
+        /// </summary>
+        /// <param name="values">The values to convert. The first value is the command, the second value the control associated to the command.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A System.Object that represents the converted value.
+        /// </returns>
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.Assert(values.Length > 1);
+
+            Debug.Assert(values[0] is RoutedCommand);
+            RoutedCommand Command = (RoutedCommand)values[0];
+            Debug.Assert(values[1] is ExtendedToolBarButton);
+            ExtendedToolBarButton Ctrl = (ExtendedToolBarButton)values[1];
+
+            return ConvertValidValues(Command, Ctrl);
+        }
+
+        private object ConvertValidValues(RoutedCommand command, ExtendedToolBarButton ctrl)
+        {
+            CommandResourceReference Reference;
+
+            switch (command)
+            {
+                case LocalizedRoutedCommand AsLocalizedRoutedCommand:
+                    Reference = AsLocalizedRoutedCommand.Reference;
+                    break;
+
+                default:
+                    Reference = ctrl.Reference;
+                    break;
+            }
+
+            ImageSource Result = Reference.GetImageSource(command.Name + ".png");
+
+            return Result;
+        }
+
+        /// <summary>
+        /// Converts a binding target value to the source binding values.
+        /// </summary>
+        /// <param name="value">The value that the binding target produces.</param>
+        /// <param name="targetTypes">The array of types to convert to. The array length indicates the number and types of values that are suggested for the method to return.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// An array of values that have been converted from the target value back to the source values.
+        /// </returns>
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return Array.Empty<object>();
+        }
+    }
+}
