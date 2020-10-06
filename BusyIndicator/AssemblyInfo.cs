530516fb378ed7230f1a3a292891bd4d8ea66e26
David Le Bansais <david.le-bansais@transdev.com>
2020-10-06 17:21:44 +0200

    Misc change. [skip ci]

---
+++ BusyIndicator/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Windows;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinitionAttribute("CustomControlsLibrary", "CustomControls")]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
