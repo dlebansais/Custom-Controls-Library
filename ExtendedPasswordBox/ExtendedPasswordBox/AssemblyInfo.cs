685fd44c5b5d63a340542eb077002e80410b7634
dlebansais <dlebansais@gmail.com>
2020-10-07 20:59:27 +0200

    Ported ExtendedPasswordBox to .NET Core project format.

---
+++ ExtendedPasswordBox/ExtendedPasswordBox/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("CustomControlsLibrary", "CustomControls")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("9595893d-5c08-4d86-9aab-f30c5ef57374")]
