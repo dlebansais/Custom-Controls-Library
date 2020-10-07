9a7440d3b85c448de1745555fb89c52acac4e738
dlebansais <dlebansais@gmail.com>
2020-10-07 21:51:51 +0200

    Ported ExtendedCommandControls project to .NET Core format. [skip ci]

---
+++ ExtendedCommandControls/ExtendedCommandControls/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("CustomControlsLibrary", "CustomControls")]
+[assembly: ThemeInfo(ResourceDictionaryLocation.SourceAssembly, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("8f2ed289-06f5-4aa9-bd8d-6266db4a168c")]
