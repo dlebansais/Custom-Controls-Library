f3dd3c2f3ecaaaace2527259d713e2c0630613b0
dlebansais <dlebansais@gmail.com>
2020-10-07 20:28:58 +0200

    Ported EnumComboBox to .NET Core project format. [skip ci]

--- EnumComboBox/EnumComboBox/Properties/AssemblyInfo.cs
+++ EnumComboBox/EnumComboBox/Properties/AssemblyInfo.cs
@@ -1,25 +1,8 @@
 ﻿using System;
-using System.Reflection;
-using System.Resources;
 using System.Runtime.InteropServices;
 using System.Windows.Markup;
 
 [assembly: XmlnsDefinitionAttribute("CustomControlsLibrary", "CustomControls")]
-
-[assembly: AssemblyTitle("Enum ComboBox")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WPF Custom Control")]
-[assembly: AssemblyCopyright("Copyright © David Le Bansais - 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
-
 [assembly: ComVisible(false)]
-
 [assembly: Guid("9c408ad3-1a59-4d93-ba5a-d745ed1d15a2")]
-
-[assembly: AssemblyVersion("1.0.1.679")]
-[assembly: AssemblyFileVersion("1.0.1.86")]
-[assembly: NeutralResourcesLanguage("en-US")]
