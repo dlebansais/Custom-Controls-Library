d0360f59f591ebd557f45a30b90ec7618f7fbdf9
dlebansais <dlebansais@gmail.com>
2020-10-07 20:23:55 +0200

    Ported EditableTextBlock to the .NET Core format. [skip ci]

--- EditableTextBlock/EditableTextBlock/Properties/AssemblyInfo.cs
+++ EditableTextBlock/EditableTextBlock/Properties/AssemblyInfo.cs
@@ -1,25 +1,7 @@
 ﻿using System;
-using System.Reflection;
-using System.Resources;
 using System.Runtime.InteropServices;
 using System.Windows.Markup;
 
 [assembly: XmlnsDefinitionAttribute("CustomControlsLibrary", "CustomControls")]
-
-[assembly: AssemblyTitle("Editable TextBlock")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WPF Custom Control")]
-[assembly: AssemblyCopyright("Copyright © David Le Bansais - 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: CLSCompliant(true)]
-
 [assembly: ComVisible(false)]
-
 [assembly: Guid("21f456c0-a605-4e37-b8dc-6e7d252a00ed")]
-
-[assembly: AssemblyVersion("1.0.1.679")]
-[assembly: AssemblyFileVersion("1.0.1.105")]
-[assembly: NeutralResourcesLanguage("en-US")]
