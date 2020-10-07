0a950a5cab884058b91f57075d38a8f933870ea6
dlebansais <dlebansais@gmail.com>
2020-10-07 21:06:38 +0200

    Ported ExtendedScrollBar project to .NET Core format.

---
+++ ExtendedScrollBar/ExtendedScrollBar/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("CustomControlsLibrary", "CustomControls")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("9352b3d3-2df0-43b8-9f40-8e9cc1b1e284")]
