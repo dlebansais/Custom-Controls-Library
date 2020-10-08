ca381e9dc9d36e42e870b3a416965eb757148913
dlebansais <dlebansais@gmail.com>
2020-10-08 13:54:26 +0200

    Ported SolutionPresenter project to .NET Core format.

---
+++ SolutionPresenter/SolutionPresenter/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinitionAttribute("CustomControlsLibrary", "CustomControls")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("ed6382cd-7bbd-43ee-b1f2-ee3d4076af41")]
