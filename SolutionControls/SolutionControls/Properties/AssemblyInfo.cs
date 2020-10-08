269b95713be800335e9caa033b3e2dfc8d42aa27
dlebansais <dlebansais@gmail.com>
2020-10-08 13:10:04 +0200

    Ported SolutionControls project to .NET Core format. [skip ci]

--- SolutionControls/SolutionControls/Properties/AssemblyInfo.cs
+++ SolutionControls/SolutionControls/Properties/AssemblyInfo.cs
@@ -1,25 +1,8 @@
 ﻿using System;
-using System.Reflection;
-using System.Resources;
 using System.Runtime.InteropServices;
 using System.Windows.Markup;
 
-[assembly: XmlnsDefinitionAttribute("CustomControlsLibrary", "CustomControls")]
-
-[assembly: AssemblyTitle("Solution Controls")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WPF Custom Control")]
-[assembly: AssemblyCopyright("Copyright © David Le Bansais - 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: XmlnsDefinition("CustomControlsLibrary", "CustomControls")]
 [assembly: CLSCompliant(true)]
-
 [assembly: ComVisible(false)]
-
 [assembly: Guid("e2867a8d-54f5-4e77-8ed1-4cd1a25f704d")]
-
-[assembly: AssemblyVersion("1.0.1.707")]
-[assembly: AssemblyFileVersion("1.0.1.130")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
