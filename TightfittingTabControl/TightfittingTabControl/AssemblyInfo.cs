d20b051f0d63ddde4244e947d58237a3333e527d
dlebansais <dlebansais@gmail.com>
2020-10-08 14:04:22 +0200

    Ported TightfittingTabControl project to .NET Core format. [skip ci]

---
+++ TightfittingTabControl/TightfittingTabControl/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("CustomControlsLibrary", "CustomControls")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("13dbc0e6-8dfe-49d3-9fa8-b523bc8fef4a")]
