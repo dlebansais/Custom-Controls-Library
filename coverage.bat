ffacdc3f413b30eac692c53e4dbf6d7f85efb195
dlebansais <dlebansais@gmail.com>
2021-05-12 12:03:18 +0200

    Typos.

--- coverage.bat
+++ coverage.bat
@@ -81,7 +81,7 @@ start cmd /c .\coverage\stop_winappdriver.bat
 call .\coverage\wait.bat 2
 
 call ..\Certification\set_tokens.bat
-if exist .\Test\Coverage-Debug_coverage.xml .\packages\%CODECOV%\tools\codecov -f ".\Test\Coverage-Debug_coverage.xml" -t "%CUSTOMCONTROLSLIBRARY_CODECOV_TOKEN%"
+if exist .\Test\Coverage-Debug_coverage.xml .\packages\%CODECOV%\tools\win7-x86\codecov -f ".\Test\Coverage-Debug_coverage.xml" -t "%CUSTOMCONTROLSLIBRARY_CODECOV_TOKEN%"
 goto end
 
 :error_console1
