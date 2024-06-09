c2a93b1e609c6e5c0bb412d66cc743fc8725cd42
David Le Bansais <dlebansais@gmail.com>
2024-06-09 16:03:30 +0200

    Added DispatcherLagMeter tests. [skip ci]

---
+++ Test/DispatcherLagMeter.Test/UnitTest.cs
@@ -0,0 +1,28 @@
+﻿namespace EnumComboBox.Test;
+
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Input;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using TestTools;
+
+[TestFixture]
+public class UnitTest
+{
+    private const string DemoAppName = "DispatcherLagMeter.Demo";
+
+    [Test]
+    public void TestDefault1()
+    {
+        DemoApp? DemoApp = DemoApplication.Launch(DemoAppName);
+        Assert.That(DemoApp, Is.Not.Null);
+
+        Window MainWindow = DemoApp.MainWindow;
+        Assert.That(MainWindow, Is.Not.Null);
+
+        Thread.Sleep(TimeSpan.FromSeconds(2));
+
+        DemoApplication.Stop(DemoApp);
+    }
+}
