07a0eb2e80a38629ee9e18726d513267e24002ae
David Le Bansais <dlebansais@gmail.com>
2024-06-13 22:48:57 +0200

    Added tests.

--- Demo/ExtendedPasswordBox.Demo/MainWindow.xaml.cs
+++ Demo/ExtendedPasswordBox.Demo/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿namespace ExtendedPasswordBox.Demo;
 
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using System.Windows;
+using CustomControls;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml.
@@ -14,5 +18,58 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         DataContext = this;
+
+        if (TestEscape == 1)
+            _ = Dispatcher.BeginInvoke(Cycle1);
+        else if (TestEscape == 2)
+            _ = Dispatcher.BeginInvoke(Cycle2);
     }
+
+    private async void Cycle1()
+    {
+        Debug.Assert(!ctrl.ShowPassword);
+        Debug.Assert(ctrl.IsPasswordEmpty);
+
+        await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(true);
+
+        isVisible.IsChecked = true;
+        Debug.Assert(ctrl.ShowPassword);
+
+        await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(true);
+
+        ctrl.Text = "test";
+        Debug.Assert(ctrl.Text == "test");
+        Debug.Assert(!ctrl.IsPasswordEmpty);
+
+        await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(true);
+
+        ExtendedPassword Password = ctrl.Password;
+        Debug.Assert(Password.Password == "test");
+        Debug.Assert(!Password.IsSecure);
+        Debug.Assert(Password.SecurePassword.Length == 0);
+
+        isVisible.IsChecked = false;
+        Debug.Assert(!ctrl.ShowPassword);
+
+        ctrl.Text = string.Empty;
+        Debug.Assert(ctrl.Text == string.Empty);
+        Debug.Assert(ctrl.IsPasswordEmpty);
+    }
+
+    private async void Cycle2()
+    {
+        Debug.Assert(!ctrl.ShowPassword);
+        Debug.Assert(ctrl.IsPasswordEmpty);
+
+        await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(true);
+
+        ExtendedPassword Password = ctrl.Password;
+        Debug.Assert(Password.IsSecure);
+        Debug.Assert(Password.SecurePassword.Length == 0);
+    }
+
+    /// <summary>
+    /// Gets or sets the test escape.
+    /// </summary>
+    public static int TestEscape { get; set; }
 }
--- Test/ExtendedPasswordBox.Test/UnitTest.cs
+++ Test/ExtendedPasswordBox.Test/UnitTest.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Threading;
 using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Definitions;
 using NUnit.Framework;
 using TestTools;
 
@@ -15,6 +16,65 @@ public class UnitTest
     public void TestDefault1()
     {
         DemoApp? DemoApp = DemoApplication.Launch(DemoAppName);
+        Assert.That(DemoApp, Is.Not.Null);
+
+        Window MainWindow = DemoApp.MainWindow;
+        Assert.That(MainWindow, Is.Not.Null);
+
+        Thread.Sleep(TimeSpan.FromSeconds(2));
+
+        DemoApplication.Stop(DemoApp);
+    }
+
+    [Test]
+    public void TestDefault2()
+    {
+        DemoApp? DemoApp = DemoApplication.Launch(DemoAppName);
+        Assert.That(DemoApp, Is.Not.Null);
+
+        Window MainWindow = DemoApp.MainWindow;
+        Assert.That(MainWindow, Is.Not.Null);
+
+        TextBox TextBox = MainWindow.FindFirstDescendant(cf => cf.ByControlType(ControlType.Edit)).AsTextBox();
+        Assert.That(TextBox, Is.Not.Null);
+
+        TextBox.Text = "test";
+
+        Thread.Sleep(TimeSpan.FromSeconds(2));
+
+        TextBox.Text = string.Empty;
+
+        Thread.Sleep(TimeSpan.FromSeconds(2));
+
+        CheckBox CheckBox = MainWindow.FindFirstDescendant(cf => cf.ByControlType(ControlType.CheckBox)).AsCheckBox();
+        Assert.That(CheckBox, Is.Not.Null);
+        Assert.That(CheckBox.IsChecked, Is.False);
+
+        CheckBox.IsChecked = true;
+
+        Thread.Sleep(TimeSpan.FromSeconds(2));
+
+        DemoApplication.Stop(DemoApp);
+    }
+
+    [Test]
+    public void TestEscape1()
+    {
+        DemoApp? DemoApp = DemoApplication.Launch(DemoAppName, "escape1");
+        Assert.That(DemoApp, Is.Not.Null);
+
+        Window MainWindow = DemoApp.MainWindow;
+        Assert.That(MainWindow, Is.Not.Null);
+
+        Thread.Sleep(TimeSpan.FromSeconds(10));
+
+        DemoApplication.Stop(DemoApp);
+    }
+
+    [Test]
+    public void TestEscape2()
+    {
+        DemoApp? DemoApp = DemoApplication.Launch(DemoAppName, "escape2");
         Assert.That(DemoApp, Is.Not.Null);
 
         Window MainWindow = DemoApp.MainWindow;
